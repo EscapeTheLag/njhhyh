e678e25a1a829720b9293191617e35c6ddf01f5d
EscapeTheLag <32941475+EscapeTheLag@users.noreply.github.com>
2019-03-30 17:59:59 -0400

    Add files via upload

---
+++ SteelSerpentsfinal.py
@@ -0,0 +1,223 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!l
+Guess a letter!o
+Guess a letter!u
+Guess a letter!s
+Guess a letter!s
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!j
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!l
+Guess a letter!o
+Guess a letter!u
+Guess a letter!s
+Guess a letter!j
+You have won!
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!a
+Guess a letter!e
+Guess a letter!k
+Guess a letter!j
+Guess a letter!f
+Guess a letter!i
+Guess a letter!o
+Guess a letter!l
+Guess a letter!n
+Guess a letter!m
+Guess a letter!j
+You have lost!
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!l
+Guess a letter!o
+Guess a letter!u
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!l
+Guess a letter!o
+Guess a letter!u
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 269, in <module>
+    elif corr==7:
+NameError: name 'corr' is not defined
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!m
+Guess a letter!n
+Guess a letter!b
+Guess a letter!v
+Guess a letter!v
+Guess a letter!c
+Guess a letter!x
+You have lost!
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!l
+Guess a letter!o
+Guess a letter!u
+Guess a letter!s
+You have won!
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!1
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 148, in <module>
+    elif corr==7:
+NameError: name 'corr' is not defined
+1
+>>> 
+>>> 1
+1
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 269, in <module>
+    elif corr==7:
+NameError: name 'corr' is not defined
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 269, in <module>
+    elif corr==7:
+NameError: name 'corr' is not defined
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 269, in <module>
+    elif corr==7:
+NameError: name 'corr' is not defined
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!a
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 226, in <module>
+    corr+=1
+NameError: name 'corr' is not defined
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Guess a letter!e
+Guess a letter!a
+Guess a letter!l
+Guess a letter!b
+Guess a letter!r
+Guess a letter!u
+Guess a letter!p
+Guess a letter!t
+Guess a letter!l
+Guess a letter!y
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+Guess a letter!s
+You have lost!
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!z
+Traceback (most recent call last):
+  File "C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py", line 106, in <module>
+    corr+=1
+NameError: name 'corr' is not defined
+>>> 
+======= RESTART: C:\Users\Daniel\Desktop\Python codes\SteelSerpents.py =======
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!1
+Guess a letter!a
+Guess a letter!b
+Guess a letter!r
+Guess a letter!z
+Guess a letter!e
+Guess a letter!l
+Guess a letter!o
+Guess a letter!u
+Guess a letter!s
+You have won!
+It is time to play bootleg hangman! Enter 1 to start, and 10 to exit!
